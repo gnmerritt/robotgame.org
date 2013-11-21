5902b9e79a40bf7101495ffd0760e6d25b83b0c7
Nathan Merritt <gnm@the-merritts.net>
2013-11-20 22:37:19 -0500

    added a smarter towards method

--- bots/fry.py
+++ bots/fry.py
@@ -1,4 +1,5 @@
 import rg
+import math
 
 class Robot(object):
     """Fry! My first crack at robotgame
@@ -38,20 +39,64 @@ class Robot(object):
            self.nearest_enemy(self.location, enemies)
         print "Nearest enemy is {d} away".format(d=enemy_distance)
         if self.should_chase(enemy_distance):
-            print "chasing enemy @ {l}".format(l=nearest_enemy.location)
-            return ['move',
-                    rg.toward(self.location, nearest_enemy.location)]
+            print "try to chase enemy @ {l}".format(l=nearest_enemy.location)
+            to_enemy = self.step_toward(self.location,
+                                        nearest_enemy.location)
+            if to_enemy:
+                return ['move', to_enemy]
 
         # Otherwise, go to the gathering point
-        to_gathering = rg.toward(self.location, self.GATHERING)
-        robot_in_way = [r for r in neighbors
-                         if r.location == to_gathering]
-        if not robot_in_way and to_gathering != self.location:
+        to_gathering = self.step_toward(self.location, self.GATHERING)
+        if to_gathering:
             print "moving towards gathering"
             return ['move', to_gathering]
 
-        print "somebody in the way - guarding"
+        print "didn't move or attack, guarding"
         return ['guard']
+
+    def step_toward(self, loc, dest):
+        """Ant navigation with basic block checking
+        Returns a move destination or None"""
+        x0, y0 = loc
+        x, y = dest
+        x_diff, y_diff = x - x0, y - y0
+        dx, dy = abs(x_diff), abs(y_diff)
+
+        print "from {l} to {d}: delta={t}"\
+          .format(l=loc, d=dest, t=(dx,dy))
+
+        forward_x = (self.add_sign(x0, x_diff), y0)
+        forward_y = (x0, self.add_sign(y0, y_diff))
+
+        can_x = not self.is_blocked(forward_x) and dx > 0
+        can_y = not self.is_blocked(forward_y) and dy > 0
+
+        # Both open, take bigger vector first
+        if can_x and can_y:
+            if dx > dy:
+                return forward_x
+            return forward_y
+        elif can_x:
+            return forward_x
+        elif can_y:
+            return forward_y
+        return None
+
+    def add_sign(self, num, diff):
+        if diff > 0:
+            return num + 1
+        elif diff < 0:
+            return num - 1
+        else:
+            return num
+
+    def is_blocked(self, dest):
+        if not dest:
+            return True
+        for loc, bot in self.g['robots'].items():
+            if loc == dest:
+                return True
+        return False
 
     def nearby_robots(self, loc):
         return [bot for loc, bot in self.g['robots'].items()
