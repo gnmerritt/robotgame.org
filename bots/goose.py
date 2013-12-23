9e6b81a8cd1253097897a06ccdfe9c206cad8bb1
Nathan Merritt <gnm@the-merritts.net>
2013-12-23 15:49:27 -0500

    initial goose commit: building hp gradient and min/maxing our move options

--- bots/goose.py
+++ bots/goose.py
@@ -1,11 +1,10 @@
 import rg
-import math
+import settings as rgs
 
 class Robot(object):
     """Goose!
     http://robotgame.org/viewrobot/8130
     """
-    MAX_HP = 50
     AVG_DAMAGE = 9
     GATHERING = rg.CENTER_POINT
 
@@ -14,75 +13,191 @@ class Robot(object):
 
     SILENT = False
 
-    def act(self, game):
+    def setup_turn(self, game):
         self.g = game
+        self.nav = Navigator(game)
+        self.enemies = [r for _, r in self.g['robots'].items()
+                        if not self.on_team(r)]
+        self.friends = [r for _, r in self.g['robots'].items()
+                        if self.on_team(r)]
+        self.__scores = {}
+        self.__hp_grad = {}
+
+    def act(self, game):
+        self.setup_turn(game)
         self.print_turn()
-        self.surrounding = rg.locs_around(self.location,
-                                          filter_out=('invalid', 'obstacle'))
-        enemies = [r for _, r in self.g.robots.items()
-                   if not self.on_team(r)]
-        self.say("{n} enemies alive this turn".format(n=len(enemies)))
-        neighbors = self.nearby_robots(self.location)
-        self.say("saw {n} nearby robots".format(n=len(neighbors)))
-        neighbor_enemies = [r for r in neighbors
-                            if not self.on_team(r)]
-        self.say("saw {n} neighbor_enemies".format(n=len(neighbor_enemies)))
 
-        # Suicide?
-        if self.should_suicide(neighbor_enemies):
-            self.say("committing suicide - {e} neighbor_enemies / {hp} hp left"
-                     .format(e=len(neighbor_enemies), hp=self.hp))
-            return ['suicide']
+        search_locs = self.nav.locs_around(self.location, radius=5)
+        destination = self.select_destination(search_locs)
 
-        to_gathering = self.step_toward(self.location, self.GATHERING)
-        # On a spawn point?
-        if self.on_spawn_unsafe():
-            if to_gathering:
-                self.say("unsafe on spawn point! running!")
-                return ['move', to_gathering]
+        if destination:
+            if destination == self.location:
+                self.say("best destination is current location, staying put")
+                target = self.choose_target(
+                    self.nearby_robots(self.location, allies=False))
+                if target:
+                    self.say("attacking robot @ {l} with hp={hp}"
+                             .format(l=target.location, hp=target.hp))
+                    return ['attack', target.location]
+                else:
+                    return ['guard']
             else:
-                self.say("can't get off spawn point, exploding")
-                return ['suicide']
-
-        # Run?
-        if self.should_run(neighbor_enemies):
-            self.say("trying to run")
-            run_to = self.find_escape(neighbors)
-            if run_to:
-                self.say("found escape {l} going to it".format(l=run_to))
-                return ['move', run_to]
-
-        # Attack?
-        target = self.choose_target(neighbor_enemies)
-        if target:
-            self.say("attacking {l}".format(l=target.location))
-            return ['attack', target.location]
-
-        # Nearby enemy?
-        nearest_enemy, enemy_distance = \
-           self.nearest_enemy(self.location, enemies)
-        self.say("Nearest enemy is {d} away".format(d=enemy_distance))
-        if self.should_chase(nearest_enemy, enemy_distance):
-            self.say("try to chase enemy @ {l}".format(l=nearest_enemy.location))
-            to_enemy = self.step_toward(self.location,
-                                        nearest_enemy.location)
-            if to_enemy:
-                return ['move', to_enemy]
-
-        # Attack where an enemy is about to move?
-        empty_attack_space = self.should_attack_empty(nearest_enemy, enemy_distance)
-        if empty_attack_space:
-            self.say("attacking {e} where enemy might move"
-                     .format(e=empty_attack_space))
-            return ['attack', empty_attack_space]
-
-        # Otherwise, go to the gathering point
-        if to_gathering:
-            self.say("moving towards gathering")
-            return ['move', to_gathering]
+                self.say("moving to {d}".format(d=destination))
+                towards = self.nav.step_toward(self.location, destination)
+                if towards:
+                    return ['move', towards]
 
         self.say("didn't move or attack, guarding")
         return ['guard']
+
+    def select_destination(self, locs):
+        best_dest = self.location
+        best_score = self.weigh_location(best_dest)
+
+        self.say("current loc has score of {b}".format(b=best_score))
+
+        for loc in locs:
+            score = self.weigh_location(loc)
+            self.say("got score {s} @ {l}".format(s=score, l=loc))
+            if score > best_score:
+                best_score = score
+                best_dest = loc
+        return best_dest
+
+    def weigh_location(self, location):
+        """Returns a number indicating how favorable moving here is"""
+        def inner(location):
+            # avoid spawn points
+            if self.nav.is_spawn_point(location) and \
+              self.incoming_spawns():
+                return -1000
+
+            enemies = self.nearby_robots(location, allies=False)
+            #friends = self.nearby_robots(location, allies=True)
+
+            hp_gradient = self.hp_gradient(location)
+
+            if len(enemies) > 0 and hp_gradient > 0:
+                for enemy in enemies:
+                    self.say("AA - see enemy and positive gradient")
+                    if enemy.hp <= self.AVG_DAMAGE * 2 or \
+                      len(self.nearby_robots(enemy.location, allies=True)) >= 1:
+                      self.say("AA - hp low or already somebody attacking")
+                      # favorable attack, go here
+                      return 1000
+
+            towards_loc = self.nav.step_toward(self.location, location)
+            if not towards_loc:
+                hp_gradient -= 50
+
+            return hp_gradient
+
+        # memoize
+        if not location in self.__scores:
+            self.__scores[location] = inner(location)
+        return self.__scores[location]
+
+    def hp_gradient(self, location):
+        def inner(location):
+            sum = 0
+            search_locs = self.nav.locs_around(location, radius=6)
+
+            for loc in search_locs:
+                if loc in self.g.robots:
+                    robot = self.g.robots[loc]
+                    dist = rg.wdist(location, robot.location)
+                    hp_weight = robot.hp / max(dist, 1)
+                    if self.on_team(robot):
+                        sum += hp_weight
+                    else:
+                        sum -= hp_weight
+            return sum
+
+        # memoize
+        if not location in self.__hp_grad:
+            self.__hp_grad[location] = inner(location)
+        return self.__hp_grad[location]
+
+    def nearby_robots(self, center, allies=True):
+        locs = rg.locs_around(center)
+        return [bot for loc, bot in self.g['robots'].items()
+                if loc in locs and
+                self.on_team(bot) == allies]
+
+    def hp_sum(self, robots):
+        sum = 0
+        for r in robots:
+            sum += r.hp
+        return sum
+
+    def choose_target(self, robots):
+        """Always attack the weakest robot"""
+        target = None
+        min_hp = rgs.settings.robot_hp
+        for r in robots:
+            if r.hp <= min_hp:
+                target = r
+                min_hp = r.hp
+        return target
+
+    def nearest_enemy(self, location, enemies):
+        nearest = None
+        nearest_dist = -1
+        for robot in enemies:
+            robot_walk_dist = rg.wdist(robot.location, location)
+            if robot_walk_dist < nearest_dist \
+                or nearest_dist == -1:
+                nearest_dist = robot_walk_dist
+                nearest = robot
+        return nearest, nearest_dist
+
+    def should_chase(self, enemy, nearest_dist):
+        weak = enemy.hp <= self.AVG_DAMAGE * 2 \
+          and enemy.hp < self.hp
+        close = nearest_dist < self.CHASE_DIST_THRESH
+        return close and weak
+
+    def should_attack_empty(self, enemy, nearest_dist):
+        if nearest_dist == 2:
+            self.say("Defensive attack: us={us},them={them}"
+                     .format(us=self.location, them=enemy.location))
+            return self.nav.step_toward(self.location, enemy.location)
+        return None
+
+    def should_run(self, enemies):
+        """Run if the enemy is about to suicide or we're outnumbered"""
+        expecting_suicide = len(enemies) == 1 and enemies[0].hp <= 8
+        dodging_suicide = self.turn() % 5 != 0
+        too_many = len(enemies) > 1
+        return (expecting_suicide and dodging_suicide) or too_many
+
+    def incoming_spawns(self):
+        """Returns True if spawning new robots this turn or next"""
+        next_spawn = self.turn() % rgs.settings.spawn_every
+        return (next_spawn == 0 or
+                next_spawn == rgs.settings.spawn_every - 1)
+
+    def should_suicide(self, neighbor_enemies):
+        return len(neighbor_enemies) * self.AVG_DAMAGE > self.hp
+
+    def on_team(self, robot):
+        return self.player_id == robot.player_id
+
+    def print_turn(self):
+        self.say("-- Goose: robot @ {l} [hp={hp}] starting turn {t} --"
+                 .format(l=self.location, hp=self.hp, t=self.turn()),
+                 prefix="")
+
+    def say(self, what, prefix="   ", level=0):
+        if not self.SILENT:
+            print prefix + what
+
+    def turn(self):
+        return self.g['turn'] + 1
+
+class Navigator(object):
+    def __init__(self, game):
+        self.g = game
 
     def step_toward(self, loc, dest):
         """Ant navigation with basic block checking
@@ -91,9 +206,6 @@ class Robot(object):
         x, y = dest
         x_diff, y_diff = x - x0, y - y0
         dx, dy = abs(x_diff), abs(y_diff)
-
-        self.say("from {l} to {d}: delta={t}"
-                 .format(l=loc, d=dest, t=(dx,dy)))
 
         forward_x = (self.add_sign(x0, x_diff), y0)
         forward_y = (x0, self.add_sign(y0, y_diff))
@@ -130,79 +242,31 @@ class Robot(object):
             return True
         return False
 
-    def nearby_robots(self, center):
-        locs = rg.locs_around(center)
-        return [bot for loc, bot in self.g['robots'].items()
-                if loc in locs]
+    def locs_around(self, location, radius=1, top_level=True):
+        around = rg.locs_around(location,
+                                filter_out=('invalid', 'obstacle'))
+        # base case
+        if radius == 1:
+            return around
 
-    def choose_target(self, robots):
-        """Always attack the weakest robot"""
-        target = None
-        min_hp = self.MAX_HP
-        for r in robots:
-            if r.hp <= min_hp:
-                target = r
-                min_hp = r.hp
-        return target
+        # recursively accumulate
+        results = []
+        for l in around:
+            results.extend(self.locs_around(l, radius=radius-1, top_level=False))
 
-    def nearest_enemy(self, location, enemies):
-        nearest = None
-        nearest_dist = -1
-        for robot in enemies:
-            robot_walk_dist = rg.wdist(robot.location, location)
-            if robot_walk_dist < nearest_dist \
-                or nearest_dist == -1:
-                nearest_dist = robot_walk_dist
-                nearest = robot
-        return nearest, nearest_dist
+        results = set(results)
 
-    def should_chase(self, enemy, nearest_dist):
-        weak = enemy.hp <= self.AVG_DAMAGE * 2 \
-          and enemy.hp < self.hp
-        close = nearest_dist < self.CHASE_DIST_THRESH
-        return close and weak
+        # don't return the original location
+        if top_level and location in results:
+            results.remove(location)
+        return results
 
-    def should_attack_empty(self, enemy, nearest_dist):
-        if nearest_dist == 2:
-            self.say("Defensive attack: us={us},them={them}"
-                     .format(us=self.location, them=enemy.location))
-            return self.step_toward(self.location, enemy.location)
-        return None
-
-    def should_run(self, enemies):
-        """Run if the enemy is about to suicide or we're outnumbered"""
-        expecting_suicide = len(enemies) == 1 and enemies[0].hp <= 8
-        dodging_suicide = self.turn() % 5 != 0
-        too_many = len(enemies) > 1
-        return (expecting_suicide and dodging_suicide) or too_many
-
-    def find_escape(self, neighbors):
-        neighbor_locs = set([n.location for n in neighbors])
-        for s in self.surrounding:
+    def find_escape(self, from_loc, neighbor_bots):
+        neighbor_locs = set([n.location for n in neighbor_bots])
+        for s in self.locs_around(from_loc):
             if not s in neighbor_locs:
                 return s
         return None
 
-    def on_spawn_unsafe(self):
-        return self.turn() % 10 == 0 and \
-          self.on_spawn_point()
-
-    def on_spawn_point(self):
-        return 'spawn' in rg.loc_types(self.location)
-
-    def should_suicide(self, neighbor_enemies):
-        return len(neighbor_enemies) * self.AVG_DAMAGE > self.hp
-
-    def on_team(self, robot):
-        return self.player_id == robot.player_id
-
-    def print_turn(self):
-        self.say("-- Goose: robot @ {l} [hp={hp}] starting turn {t} --"
-                 .format(l=self.location, hp=self.hp, t=self.turn()))
-
-    def say(self, what):
-        if not self.SILENT:
-            print what
-
-    def turn(self):
-        return self.g['turn'] + 1
+    def is_spawn_point(self, loc):
+        return 'spawn' in rg.loc_types(loc)
